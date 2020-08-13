30a89d93eca6414a7f9f167b616d8002d84f4a15
ntoine08 <j.antoine689@laposte.net>
2020-08-13 14:44:49 +0200

    1er commit

---
+++ chapitres/readme.md
@@ -0,0 +1,4 @@
+php -S localhost:8000 -t public
+
+http://localhost:8000/blog
+http://localhost:8000/blog/category
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php 
+require '../vendor/autoload.php';
+$router = new AltoRouter();
+
+define('VIEW_PATH', dirname(__DIR__). '/views');
+
+$router->map('GET', '/blog', function() {
+    require VIEW_PATH . '/post/index.php';
+});
+
+$router->map('GET', '/blog/category', function() {
+    require VIEW_PATH . '/category/show.php';
+});
+
+$router->map('GET', '/', function() {
+    require VIEW_PATH . '/home/index.php';
+});
+
+$match = $router->match();
+$match['target']();
